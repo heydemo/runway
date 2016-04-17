2439f7730310f0f2e76d80e0c17363213935320c
John De Mott <jdemott@opentactics.com>
2016-04-16 19:20:30 -0500

    Fixing babel loader issue in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,11 +16,8 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        plugins: ['transform-runtime'],
-        presets: ['es2015', 'stage-0']
-
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
       }
     ]
   },
